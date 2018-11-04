20c9eb642cb9a1302396f5258c98ed5843bcb1f0
Alexander Stoychev <stoychev92@yahoo.co.uk>
2018-11-04 21:30:23 +0200

    Fixed dropdown on focus color and added base champion controller, model and view

---
+++ ESportStatistics/ESportStatistics.Web/Areas/Statistics/Models/ChampionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ESportStatistics.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESportStatistics.Web.Areas.Statistics.Models
+{
+    public class ChampionViewModel
+    {
+
+        public ChampionViewModel()
+        {
+        }
+
+        public ChampionViewModel(Champion champion)
+        { 
+        //{
+        //    this.Content = comment.Content;
+        //    this.Likes = comment.Likes;
+        //    this.Author = comment.Author.UserName;
+        //    this.CreatedOn = comment.CreatedOn ?? DateTime.Now;
+        }
+
+        //da naredq tuka neshtata za viewmodela
+    }
+}
