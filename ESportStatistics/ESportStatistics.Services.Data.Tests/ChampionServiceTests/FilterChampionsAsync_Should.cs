36307b5ae3e0f04b6d38ee789d4f728e2a6458da
Alex <aleksander.peich@gmail.com>
2018-11-10 20:31:07 +0200

    Add champion tests bug fix

--- ESportStatistics/ESportStatistics.Services.Data.Tests/ChampionServiceTests/FilterChampionsAsync_Should.cs
+++ ESportStatistics/ESportStatistics.Services.Data.Tests/ChampionServiceTests/FilterChampionsAsync_Should.cs
@@ -11,6 +11,48 @@ namespace ESportStatistics.Services.Data.Tests.ChampionServiceTests
     [TestClass]
     public class FilterChampionsAsync_Should
     {
+        [TestMethod]
+        public async Task ThrowArgumentNullException_WhenPassedNullSortOrder()
+        {
+            // Arrange
+            Mock<IPandaScoreClient> pandaScoreClientMock = new Mock<IPandaScoreClient>();
+            Mock<DataContext> dataContextMock = new Mock<DataContext>();
+
+            string invalidSortOrder = null;
+            string validFilter = string.Empty;
+            int validPageNumber = 1;
+            int validPageSize = 10;
+
+            ChampionService SUT = new ChampionService(
+                pandaScoreClientMock.Object,
+                dataContextMock.Object);
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(
+                () => SUT.FilterChampionsAsync(invalidSortOrder, validFilter, validPageNumber, validPageSize));
+        }
+
+        [TestMethod]
+        public async Task ThrowArgumentNullException_WhenPassedNullFilter()
+        {
+            // Arrange
+            Mock<IPandaScoreClient> pandaScoreClientMock = new Mock<IPandaScoreClient>();
+            Mock<DataContext> dataContextMock = new Mock<DataContext>();
+
+            string validSortOrder = string.Empty;
+            string invalidFilter = null;
+            int validPageNumber = 1;
+            int validPageSize = 10;
+
+            ChampionService SUT = new ChampionService(
+                pandaScoreClientMock.Object,
+                dataContextMock.Object);
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(
+                () => SUT.FilterChampionsAsync(validSortOrder, invalidFilter, validPageNumber, validPageSize));
+        }
+
         [DataTestMethod]
         [DataRow(0)]
         [DataRow(-10)]
